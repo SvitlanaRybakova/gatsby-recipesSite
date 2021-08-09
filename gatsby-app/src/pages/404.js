c18686aff2b233a1e4f6bbe5b14e7e50a7c775e0
Svitlana <devop.edu@gmail.com>
2021-08-09 16:55:35 +0200

    init project

---
+++ gatsby-app/src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout'
+
+const Error = () => {
+  return (
+    <Layout>
+      <h1>Sorry, we could not find the page that you looking for</h1>
+    </Layout>
+  )
+}
+
+export default Error;
