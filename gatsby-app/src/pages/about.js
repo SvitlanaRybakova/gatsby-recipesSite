3ee96184e66a66d2ce95fbc9029501ccc0ca9c49
Svitlana <devop.edu@gmail.com>
2021-08-10 15:40:12 +0200

    add StaticImage

--- gatsby-app/src/pages/about.js
+++ gatsby-app/src/pages/about.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-const about = () => {
+const About = () => {
   return (
     <Layout>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book</h2>
+            <p>
+              Coloring book forage glossier letterpress heirloom before they
+              sold out you probably haven't heard of them banh mi biodiesel
+              chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contacts" className="btn">
+              Contacts
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
       <h1>about page</h1>
     </Layout>
   )
 }
 
-export default about
-
+export default About
